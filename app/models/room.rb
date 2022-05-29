4deed0aa29b76706243d1642c769b924597e09ab
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2022-05-29 13:51:21 -0300

    Add Room spec

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,4 @@
 class Room < ApplicationRecord
   belongs_to :game
-  belongs_to :user
+  belongs_to :owner, class_name: User.name, foreign_key: :user_id
 end
---
+++ spec/models/room_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  context 'Relationships' do
+    it { is_expected.to belong_to(:owner).with_foreign_key(:user_id).class_name(User.name) }
+    it { is_expected.to belong_to(:game) }
+  end
+end
