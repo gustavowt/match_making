07957a194317dcfe9ec6349497cf1042ec221961
Gustavo Warmling Teixeira <gustavo@trustedhealth.com>
2022-06-28 22:13:24 -0300

    Fix player broadcaster

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,5 +3,5 @@ class Player < ApplicationRecord
   belongs_to :room
 
   after_create_commit { broadcast_append_to "players_room_#{room_id}", target: "players_room_#{room_id}" }
-  after_destroy_commit { broadcast_remove_to "players_room_#{room_id}", target: "players_room_#{room_id}" }
+  after_destroy_commit { broadcast_remove_to "player_#{id}" }
 end
